5aa36b76a7f7ad2fdede1f11afe57a3c8003653e
CHKAFF <53139664+CHKAFF@users.noreply.github.com>
2023-04-13 14:08:18 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,15 @@
-import pandas as pd
 import numpy as np
+from scipy.stats import binom_test
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 303247798
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    control_conversion = x_success / x_cnt
+    test_conversion = y_success / y_cnt
+
+    p_value = binom_test(y_success, y_cnt, control_conversion)
+
+    return p_value < 0.05
